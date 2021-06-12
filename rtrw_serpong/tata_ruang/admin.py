b96de7b9ad195f421675630403e1724347a163f7
tibrahim <taufiq.ibrahim@bizzy.co.id>
2021-06-12 09:27:03 +0700

    update tata_ruang models and admin

--- rtrw_serpong/tata_ruang/admin.py
+++ rtrw_serpong/tata_ruang/admin.py
@@ -5,6 +5,13 @@ from django.contrib import admin
 from . import models as tata_ruang_models
 
 # admin.site.register(tata_ruang_models.BidangTanah, LeafletGeoAdmin)
+
+
 @admin.register(tata_ruang_models.BidangTanah)
 class BidangTanahAdmin(BaseAuditedLeafletGeoModelAdmin):
+    pass
+
+
+@admin.register(tata_ruang_models.Bangunan)
+class BangunanAdmin(BaseAuditedLeafletGeoModelAdmin):
     pass
--- rtrw_serpong/tata_ruang/models.py
+++ rtrw_serpong/tata_ruang/models.py
@@ -18,3 +18,26 @@ class BidangTanah(BaseModel):
     @property
     def picture_url(self):
         return self.picture.url
+
+
+class Bangunan(BaseModel):
+    bidang_tanah = models.ForeignKey(
+        BidangTanah, on_delete=models.PROTECT, null=False)
+    alamat = models.CharField(max_length=255)
+    geom = PolygonField(null=True)
+
+    # pemilik = TODO foreign to biodata
+
+    # uu-no-28-tahun-2002-bangunan-gedung BAB III FUNGSI BANGUNAN GEDUNG Pasal 5 direkomendasikan menjadi data master
+    fungsi = models.ForeignKey(
+        'master.FungsiBangunan', on_delete=models.PROTECT)
+
+    class Meta:
+        verbose_name = 'Bangunan'
+        verbose_name_plural = 'Bangunan'
+
+    def __str__(self) -> str:
+        if self.alamat:
+            return self.alamat
+        else:
+            return self.pk
